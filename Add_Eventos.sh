795fd784a2a2d3c7a01acee7544562afe89069b8
Sergio Godinho <54249673+SirTomates@users.noreply.github.com>
2025-12-10 11:36:59 +0000

    adicionada a base de dados contactos e eventos. As funções adicionar contactos e list dos mesmos e verificaço~es e validacões

--- Add_Eventos.sh
+++ Add_Eventos.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/bash
 
 
-//Menu principal do script onde o utilizador ira navegar 
+# Bases de dados dos eventos e contactos
+
+ARQ_Eventos="eventos.db"
+ARQ_Contactos="contactos.db"
+
+
+
+
+
+
+
+# Menu principal do script onde o utilizador ira navegar 
 
 menu_principal() {
     while true; do
@@ -25,3 +36,43 @@ menu_principal() {
         esac
     done
 }
+
+
+# Funcao para adicionar contactos
+
+adicionar_contato() {
+
+    echo "- Nome do novo contacto =>"; read nome
+        if [ -z "$nome" ]; then
+            menu_principal
+        fi
+    echo "- Número do contacto =>"; read num
+        if [ -z "$num" ]; then
+            menu_principal
+        fi
+   
+    if ! [[ "$num" =~ ^9[0-9]{8}$ ]]; then
+        echo "************************";
+        echo "    Número invalido ";
+        echo "************************";
+        $nome=null
+        adicionar_contato
+    fi
+
+    echo "Deseja guardar o contacto?(s/n)"; read conf
+    [[ "$conf" != "s" ]] && { echo "Cancelado."; return; }
+
+    echo "$nome|$numero" >> "$ARQ_Contactos"
+    echo "Contato guardado."
+
+     clear
+}
+
+listar_contatos() {
+    echo "--- Lista Telefónica ---"
+    nl -w2 -s". " "$ARQ_Contactos" | sed 's/|/ - /'
+}
+
+
+clear
+menu_principal
